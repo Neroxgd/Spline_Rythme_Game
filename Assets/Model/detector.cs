728cc699a1799860fc7ab84f27be856aa4b45953
frenale59 <gregoire.adias@gmail.com>
2022-10-04 16:24:06 +0200

    ff

--- Assets/Model/detector.cs
+++ Assets/Model/detector.cs
@@ -4,34 +4,36 @@ using UnityEngine;
 
 public class detector : MonoBehaviour
 {
-
+    [SerializeField] float raycastLength = 4f;
+    Vector3 OriginPoint;   
+    
+    Vector3 Direction;
    
-    private void Start() {
+    public void Start() 
+    {
         
-       
+      /*  Vector3 OriginPoint = new Vector3(transform.localPosition.x,transform.localPosition.y,transform.localPosition.z);
+        OriginPoint +=  Vector3.left * 4;
 
-
-
-
-
+        Vector3 RayEndPoint =  new Vector3(transform.localPosition.x,transform.localPosition.y,transform.localPosition.z);
+        RayEndPoint -= Vector3.right * 4; */
     }
 
    public void FixedUpdate() {
     
-
-        
-        Vector3 OriginRay = transform.position ;
+        OriginPoint = transform.position + raycastLength * transform.up;
+        Direction = -transform.up;
 
             
    
-        Ray ray = new Ray(OriginRay, Vector3.down);
+        Ray ray = new Ray(OriginPoint, Direction);
         RaycastHit hit;
 
 
-        if(Physics.Raycast(ray, out hit, 10))
-        {
-            Debug.DrawLine(ray.origin,hit.point,Color.green);
-        }
+        Physics.Raycast(ray, out hit, raycastLength);
+        
+        Debug.DrawRay(ray.origin,Direction*raycastLength,Color.green);
+        
         
 
 
